4750bf015a6c67eb92863ee59bf13cdcbc7f474d
Vladyslav <31669338+vladikbrumar@users.noreply.github.com>
2023-04-23 16:12:04 -0700

    Update script.js

--- webapp/script.js
+++ webapp/script.js
@@ -1,8 +1,20 @@
 const slides = document.querySelectorAll('.slide');
 const nextBtn = document.querySelector('.btn-next');
 const prevBtn = document.querySelector('.btn-prev');
+const clickBtn = document.querySelector('.btn-click-me');
+const navBtns = document.querySelector('.nav-btns');
 
 const pins = document.querySelectorAll('.pin');
+
+if(slides.length === 1){
+    nextBtn.style.display = 'none';
+    prevBtn.style.display = 'none';
+    clickBtn.style.display = 'block';
+    navBtns.style.justifyContent = 'center';
+} else {
+    nextBtn.style.display = 'block';
+    prevBtn.style.display = 'block';
+}
 
 slides.forEach(function(slide, index) {
     slide.style.left = `${index * 100}%`;
@@ -18,6 +30,10 @@ nextBtn.addEventListener('click', () => {
 prevBtn.addEventListener('click', () => {
     counter--;
     carousel();
+})
+
+clickBtn.addEventListener('click', () => {
+    alert('Ты красотка!');
 })
 
 function carousel() {
